437d5cec313e236a2df17166a97beb200e4b8002
Peteris Dambergs <robertspeterisd@gmail.com>
2023-12-03 11:07:19 +0200

    Book class created, borrow function implemented

---
+++ library.py
@@ -0,0 +1,17 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.is_borrowed = False
+
+    def borrow(self):
+        if self.is_borrowed:
+            print(f"Failure, the book {self.title} is unavailable")
+        else:
+            self.is_borrowed = True
+            print(f"Success, the book {self.title} is now borrowed")
+
+
+newbook = Book("Harry Potter", "JK")
+newbook.borrow()
+newbook.borrow()
