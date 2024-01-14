05f5a9f420b554be9dd2084664f287cfa2157a8e
Peteris Dambergs <robertspeterisd@gmail.com>
2024-01-14 12:48:39 +0200

    peteris_main.py created, entries extracted from csv file

--- file_handling/peteris_main.py
+++ file_handling/peteris_main.py
@@ -1,5 +1,19 @@
+def get_entries_from_csvfile(csvfile):
+    with open(csvfile, "r") as f:
+        headers = f.readline().strip().split(",")
+        lines = f.readlines()
+
+        entries = []
+        for line in lines:
+            line = line.strip().split(",")
+            entry = {headers[0]: line[0], headers[1]: line[1], headers[2]: line[2], headers[3]: line[3]}
+            entries.append(entry)
+        return entries
+
+
 def main():
-    pass
+    entries = get_entries_from_csvfile("data.csv")
+    
 
 
 if __name__ == "__main__":
