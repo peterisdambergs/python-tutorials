87f0e02903a36cdcbc63b03b8f645a40eb75880e
Eduards Blumentals (TAABLED2) <eduards.blumentals@swisscom.com>
2024-01-21 13:01:35 +0200

    New lines added to data.csv and function that prints names by country implemented.

--- file_handling/eduards_main.py
+++ file_handling/eduards_main.py
@@ -9,20 +9,32 @@ def get_entries_from_csv_file(csv_file):
 
 def print_average_age(entries):
     ages = [int(entry.get("Age")) for entry in entries]
-    print(f"Average age is {sum(ages)/len(ages)}")
+    print(f"Average age is {round(sum(ages)/len(ages), 2)}")
 
 
-def print_names_by_occupation(entries):
+def print_names_by_occupation_for_all_occupations(entries):
     occupations = sorted(set([entry.get("Occupation") for entry in entries]))
     for occupation in occupations:
         name_list = [entry.get("Name") for entry in entries if entry.get("Occupation") == occupation]
         print(f"{occupation}: {name_list}")
 
 
+def add_line_to_csv_file(name, age, country, occupation):
+    with open("data.csv", 'a') as f:
+        f.write(f"\n{name},{age},{country},{occupation}")
+
+
+def print_names_by_country(entries, country):
+    names_list = [entry.get("Name") for entry in entries if entry.get("Country") == country]
+    print(f"{country}: {names_list}")
+
+
 def main():
     entries = get_entries_from_csv_file("data.csv")
-    print_average_age(entries)
-    print_names_by_occupation(entries)
+    # print_average_age(entries)
+    # print_names_by_occupation(entries)
+    # add_line_to_csv_file("Peteris", 18, "Italy", "Student")
+    print_names_by_country(entries, "UK")
 
 
 if __name__ == "__main__":
--- file_handling/task.md
+++ file_handling/task.md
@@ -4,8 +4,8 @@
 - **Count the Entries**: Count how many people are listed in the file. (Done)
 - **Average Age**: Calculate the average age of the people in the file. (Done)
 - **List by Occupation**: Create a list of names for each occupation. (Done)
-- **Add a New Entry**: Write a function to add a new entry to the file (e.g., Fiona, 29, France, Lawyer).
-- **Search by Country**: Write a function that finds all people from a specific country (e.g., find all people from "Germany").
+- **Add a New Entry**: Write a function to add a new entry to the file (e.g., Fiona, 29, France, Lawyer). (Done)
+- **Search by Country**: Write a function that finds all people from a specific country (e.g., find all people from "Germany"). (Done)
 - **Update an Entry**: Update the age of a specific person and save the changes to the file.
 - **Sort by Age**: Create a new file that is sorted by age in ascending order.
 - **Filter by Age Range**: Write a function to create a new file that only includes people within a specified age range (e.g., 25 to 30 years old).
