82d87ad0de7dab4b13794da983efcef96cace39e
Eduards Blumentals (TAABLED2) <eduards.blumentals@swisscom.com>
2024-01-21 12:36:43 +0200

    Getting entries from csv more efficiently.

--- file_handling/eduards_main.py
+++ file_handling/eduards_main.py
@@ -1,29 +1,9 @@
-def get_headers_and_lines_from_csv_file(csv_file):
+def get_entries_from_csv_file(csv_file):
     with open(csv_file, "r", encoding="utf-8") as f:
         headers = f.readline().strip().split(",")
         lines = f.readlines()
 
-    return headers, lines
-
-
-def get_entry_from_line(line, headers):
-    line_list = line.strip().split(",")
-
-    entry = {}
-    for key, value in zip(headers, line_list):
-        entry[key] = value
-
-    return entry
-
-
-def get_entries_from_csv(csv_file):
-    headers, lines = get_headers_and_lines_from_csv_file(csv_file)
-    entries = []
-
-    for line in lines:
-        entry = get_entry_from_line(line, headers)
-        entries.append(entry)
-
+    entries = [{k: v for k, v in zip(headers, line.strip().split(","))} for line in lines]
     return entries
 
 
@@ -34,14 +14,13 @@ def print_average_age(entries):
 
 def print_names_by_occupation(entries):
     occupations = sorted(set([entry.get("Occupation") for entry in entries]))
-
     for occupation in occupations:
         name_list = [entry.get("Name") for entry in entries if entry.get("Occupation") == occupation]
         print(f"{occupation}: {name_list}")
 
 
 def main():
-    entries = get_entries_from_csv("data.csv")
+    entries = get_entries_from_csv_file("data.csv")
     print_average_age(entries)
     print_names_by_occupation(entries)
 
