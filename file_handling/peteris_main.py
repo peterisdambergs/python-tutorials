ddf00499335b564941b7b531e846dd1eeb367635
Peteris Dambergs <robertspeterisd@gmail.com>
2024-02-11 12:55:48 +0200

    Sort by age and filter by age implemented in peteris file

--- file_handling/peteris_main.py
+++ file_handling/peteris_main.py
@@ -1,3 +1,7 @@
+formatted_header = lambda entry: ','.join(entry.keys())
+formatted_entry = lambda entry: f"\n{','.join([str(e) for e in entry.values()])}"
+
+
 def get_entries_from_csvfile(csvfile):
     with open(csvfile, "r") as f:
         headers = f.readline().strip().split(",")
@@ -42,12 +46,32 @@ def update_csvfile_age_by_name(csvfile, name, age):
             f.write(f"\n{','.join(entry.values())}")
 
 
+def sort_entries_by_age(entries):
+    sorted_entries = sorted(entries, key=lambda entry: entry.get("Age"))
+    with open("data_by_age.csv", "w") as f:
+        f.write(formatted_header(entries[0]))
+        for entry in sorted_entries:
+            f.write(formatted_entry(entry))
+
+
+def filter_by_age_range(entries, min, max, to_sort=True):
+    if to_sort:
+        entries = sorted(entries, key=lambda entry: entry.get("Age"))
+    with open(f"data_filtered_from_{min}_to_{max}.csv", "w") as f:
+        f.write(formatted_header(entries[0]))
+        for entry in entries:
+            if max >= entry.get("Age") >= min:
+                f.write(formatted_entry(entry))
+
+
 def main():
     entries = get_entries_from_csvfile("data.csv")
     # print_average_age(entries)
     # print_names_by_occupation(entries)
     # print_names_by_country(entries, "Germany")
-    update_csvfile_age_by_name("data.csv", "Erik", 25)
+    # sorted_entries = sorted(entries, key=lambda entry: entry.get("Age"))
+    # sort_entries_by_age(entries)
+    # filter_by_age_range(entries, 20, 35, False)
 
 
 if __name__ == "__main__":
