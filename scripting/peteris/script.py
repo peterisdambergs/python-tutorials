c0a7ef5bb264f5d8b374d0aa466c4ae57e9af444
Peteris Dambergs <robertspeterisd@gmail.com>
2024-03-10 13:08:31 +0200

    Text, link, image extraction from article implemented in peteris script.py

--- scripting/peteris/script.py
+++ scripting/peteris/script.py
@@ -13,13 +13,26 @@ def get_html_from_source(url, file_name):
         return f.read()
 
 
+def get_link_image_text_from_article(article):
+    link = f"https://delfi.lv{article.a.get('href')}"
+    image = article.img.get("src")
+    text = article.get_text()
+
+    return link, image, text
+
 def main():
     html = get_html_from_source("https://www.delfi.lv/bizness/biznesa_vide", "source.html")
     soup = BeautifulSoup(html, "lxml")
 
     articles = soup.find_all("article")
+    # for article in articles:
+    #     print(article)
+
     for article in articles:
-        print(article)
+        link, image, text = get_link_image_text_from_article(article)
+        print(text)git gi
+        print(link)
+        print(f"{image}\n")
 
 
 if __name__ == "__main__":
