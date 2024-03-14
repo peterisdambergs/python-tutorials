af1cb5fe646dcada4423f4d5855d74c911fdee57
Eduards Blumentals (TAABLED2) <eduards.blumentals@swisscom.com>
2024-03-14 19:06:17 +0200

    Table of contents creation added.

--- scripting/eduards/script.py
+++ scripting/eduards/script.py
@@ -2,10 +2,8 @@ import os, re, requests
 
 from bs4 import BeautifulSoup
 
-from PIL import Image
 
-
-def get_html_from_source(url="https://www.delfi.lv/bizness/biznesa_vide", file_name="source.html"):
+def get_html_from_source(url, file_name):
     if not os.path.exists(file_name):
         with open(file_name, "w", encoding="utf-8") as f:
             r = requests.get(url)
@@ -17,32 +15,51 @@ def get_html_from_source(url="https://www.delfi.lv/bizness/biznesa_vide", file_n
     return html
 
 
-def get_text_img_from_article(article):
-    text = article.get_text()
-    link = f"https://delfi.lv{article.a.get('href')}"
-    img = article.img.get("src")
+def create_article_file_path_based_on_article_name_and_category(name, category):
+    forbidden_char_pattern = r"[^a-zA-Z0-9 ()_\-,.*]+"
+    file_name = re.sub(forbidden_char_pattern, "", name)[:35] + "***.md"
+    print(os.path.join(os.getcwd(), category, file_name).replace("\\", "\\\\"))
+    return os.path.join(os.getcwd(), category, file_name).replace("\\", "\\\\")
 
-    return text, link, img
 
+def get_formatted_article(article, category):
+    name = article.get_text()
 
-def create_file_from_article(dir, file_name, article):
-    text, link, img = get_text_img_from_article(article)
+    article = {
+        "name": name,
+        "link": f"https://delfi.lv{article.a.get('href')}",
+        "img": article.img.get("src"),
+        "file_path": create_article_file_path_based_on_article_name_and_category(name, category)
+    }
 
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-
-    with open(os.path.join(dir, file_name), "w", encoding="utf-8") as f:
-        f.write(f"Text: {text}\n")
-        f.write(f"Link: {link}\n")
-        f.write(f"Image: {img}\n")
+    return article
 
 
 def main():
-    html = get_html_from_source()
-    soup = BeautifulSoup(html, "lxml")
+    root_url = "https://www.delfi.lv/bizness/"
 
-    for num, article in enumerate(soup.find_all("article")):
-        create_file_from_article("articles", f"article_{num+1}.txt", article)
+    category_dict = {
+        "biznesa_vide": "Economy",
+        "bankas_un_finanses": "Finance",
+        "tehnologijas": "Tech",
+        "nekustamais-ipasums": "Real Estate",
+        "pasaule": "World"
+    }
+
+    with open("articles.md", "w", encoding="utf-8") as f:
+        f.write("# Articles\n")
+        for category in category_dict:
+            f.write(f"### {category_dict.get(category)}\n")
+
+            if not os.path.exists(category): os.mkdir(category)
+
+            html = get_html_from_source(f"{root_url}/{category}", f"{category}.html")
+            soup = BeautifulSoup(html, "lxml")
+
+            for num, article in enumerate(soup.find_all("article")):
+                formatted_article = get_formatted_article(article, category)
+                f.write(f"- [{formatted_article.get('name')}]({formatted_article.get('file_path')})\n")
+                if num == 4: break
 
 
 if __name__ == "__main__":
