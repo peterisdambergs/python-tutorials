ef1dd82b666ae1d3a0676b5b4fd5ebe5cdf126b2
Peteris Dambergs <robertspeterisd@gmail.com>
2024-06-23 12:57:33 +0300

    imdb code finished

--- imdb/script.py
+++ imdb/script.py
@@ -13,24 +13,31 @@ def create_toc_file(toc_name, base_url, categories, year_list):
         f.write("<h1>Table of Contents</h1>\n")
         for category in categories:
             for year in year_list:
-                link = f"{base_url}/chart/moviemeter/?ref_=nv_mv_mpm&genres={category}&year={year[0]}%2C{year[1]}"
-                f.write(f"<h3><a href={link}>{category.capitalize()}: {year[0]} - {year[1]}</a></h3>\n")
+                category_link, movie_links = get_category_and_movie_links(base_url, category, year)
+                f.write(f"<h1><a href={category_link}>{category.capitalize()}: {year[0]} - {year[1]}</a></h1>\n")
+                for movie_link in movie_links:
+                    movie = get_formatted_movie(movie_link)
+                    f.write(f"<h3><a href={movie_link}>{movie.get('title')}</a></h3>\n")
+                    f.write(f"<img src='{movie.get('img_link')} alt=''>")
+                    f.write(f"<p><i>{movie.get('rating')}</i></p>")
+                    f.write(f"<p>{movie.get('description')}</p>")
+                    f.write("<hr>")
 
 
 def get_movie_link_from_movie_element(base_url, movie):
     return f"{base_url}{movie.find('a', attrs={'class': 'ipc-title-link-wrapper'}).get('href')}"
 
 
-def get_movie_links(base_url, category, year):
-    url = f"{base_url}/chart/moviemeter/?ref_=nv_mv_mpm&genres={category}&year={year[0]}%2C{year[1]}"
-    html = get_html_from_url(url)
+def get_category_and_movie_links(base_url, category, year):
+    category_link = f"{base_url}/chart/moviemeter/?ref_=nv_mv_mpm&genres={category}&year={year[0]}%2C{year[1]}"
+    html = get_html_from_url(category_link)
     soup = BeautifulSoup(html, "lxml")
     movies = soup.find_all('li', attrs={'class': 'ipc-metadata-list-summary-item'})
 
-    return [get_movie_link_from_movie_element(base_url, movie) for movie in movies]
+    return category_link, [get_movie_link_from_movie_element(base_url, movie) for movie in movies][:3]
 
 
-def get_formatted_movie(movie_link, base_url):
+def get_formatted_movie(movie_link):
     soup = BeautifulSoup(get_html_from_url(movie_link), "lxml")
 
     movie = {
@@ -50,12 +57,7 @@ def main():
     categories = ["drama", "thriller", "action"]
     year_list = [(2004, 2019), (2020, 2024)]
 
-    movie_links = get_movie_links(base_url, categories[0], year_list[0])
-
-    movie = get_formatted_movie(movie_links[0], base_url)
-    print(movie)
-
-    # create_toc_file("imdb_toc.html", base_url, categories, year_list)
+    create_toc_file("imdb_toc.html", base_url, categories, year_list)
 
 
 if __name__ == "__main__":
